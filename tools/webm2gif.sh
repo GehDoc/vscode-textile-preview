1ccf850b3c68bb0585fd08bebcb892eae6235d10
Gérald Hameau <GehDoc@users.noreply.github.com>
2020-03-01 23:33:47 +0100

    [DOC] small screencast correction

--- tools/webm2gif.sh
+++ tools/webm2gif.sh
@@ -14,7 +14,7 @@ palettename="/tmp/"$(basename "$file")".png"
 
 
 # 3840x2160 -> crop top+bottom bars -> scale to 1280x~720 (keep ar), 10 fps
-ffmpeg -y -i "$file" -filter:v "crop=iw:ih-120:0:50,scale=1280:-2" -r 10 "$reducedname"
+ffmpeg -y -i "$file" -filter:v "crop=iw:ih-120:0:53,scale=1280:-2" -r 10 "$reducedname"
 ffmpeg -y -i "$reducedname" -vf palettegen "$palettename"
 ffmpeg -y -i "$reducedname" -i "$palettename" -filter_complex paletteuse -r 10 "$file.gif"
 rm "$palettename" "$reducedname"
