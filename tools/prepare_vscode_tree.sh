1ee8a4f397829a9f3a712169e862973ed4a3a46e
Gérald Hameau <GehDoc@users.noreply.github.com>
2020-08-16 22:22:05 +0200

    [INTERNAL] Prepare update to vscode-1.48.0

--- tools/prepare_vscode_tree.sh
+++ tools/prepare_vscode_tree.sh
@@ -7,7 +7,7 @@
 # After running this tool, you can compare ./ and ./tools/tmp/out/ to gather
 # new features.
 
-VSCODE_VERSION_GIT_TAG=1.47.3
+VSCODE_VERSION_GIT_TAG=1.48.0
 
 . $(dirname $0)/log.sh
 
