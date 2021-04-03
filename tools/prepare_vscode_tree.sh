04fccf0239b81bb50fd1ce3505e0286f62be2e3b
Gérald Hameau <GehDoc@users.noreply.github.com>
2021-04-03 21:24:02 +0200

    [INTERNAL] prepare update to vscode-1.55.0

--- tools/prepare_vscode_tree.sh
+++ tools/prepare_vscode_tree.sh
@@ -7,7 +7,7 @@
 # After running this tool, you can compare ./ and ./tools/tmp/out/ to gather
 # new features.
 
-VSCODE_VERSION_GIT_TAG=1.54.1
+VSCODE_VERSION_GIT_TAG=1.55.0
 
 . $(dirname $0)/log.sh
 
